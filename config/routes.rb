22492e6617c2141b914d98ab18d2aff0d360336c
stephane coqueret <stephane.coqueret@gmail.com>
2022-05-24 11:39:09 +0200

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, except: [:destroy] do
+    resources :bookings, except: [:destroy]
+    resources :yachts, except: [:destroy]
+  end
+  resources :users, only: :destroy
+  resources :bookings, only: :destroy
+  resources :yachts, only: :destroy
 end
