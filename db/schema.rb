a0346e25915eb8a58079872803f93559ebc70f91
mathia pagani <mathiapagani@gmail.com>
2022-05-24 17:55:26 +0200

    creating route top_yacht=> root + adding param stars table yachts + adding method top to Yacht controller + creating page top

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_105055) do
+ActiveRecord::Schema.define(version: 2022_05_24_153534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_105055) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "stars"
     t.index ["user_id"], name: "index_yachts_on_user_id"
   end
 
