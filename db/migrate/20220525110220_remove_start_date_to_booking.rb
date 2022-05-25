5b7a5f5ab146a1fc2effbf9b19f1a7410f6be749
stephane coqueret <stephane.coqueret@gmail.com>
2022-05-25 13:12:33 +0200

    migration

---
+++ db/migrate/20220525110220_remove_start_date_to_booking.rb
@@ -0,0 +1,4 @@
+class RemoveStartDateToBooking < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
---
+++ db/migrate/20220525110236_remove_end_date_to_booking.rb
@@ -0,0 +1,4 @@
+class RemoveEndDateToBooking < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
