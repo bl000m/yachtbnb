6146cdd0c3358c0c29cc1bd712f70ff4e93ff651
mathia pagani <mathiapagani@gmail.com>
2022-05-26 11:34:12 +0200

    wip

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -3,17 +3,4 @@ class Yacht < ApplicationRecord
   has_many :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
-  def self.search_by_rate(search)
-    if search
-      search_rate = search.to_i
-      if self.where({stars: 0..search_rate})
-        self.where({stars: 0..search_rate}).sort_by{|yacht| yacht.stars}
-      else
-        Yacht.all
-      end
-    else
-      Yacht.all
-    end
-  end
 end
