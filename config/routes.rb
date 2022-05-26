8ea109ca4aa331565275042991a54e1b138e4c75
mathia pagani <mathiapagani@gmail.com>
2022-05-26 16:12:54 +0200

    new root, no more top - search bar and map in the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'yachts#top', as: :top_yachts
+  root to: 'yachts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :show
-  # get "yachts/top", to: "yachts#top", as: :top_restaurants
-  resources :yachts do
+  resources :yachts, except: :index do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, except: [:new, :create, :index]
