e4b749ac099a839f86a5f2f847d84c811c4223b0
cedric arnous <cedric.arnous@gmail.com>
2022-05-27 13:14:57 +0200

    navbar ok

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,6 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("hello from navbar_controller!")
+    const navigation = document.querySelector('nav');
+    const logo = document.querySelector('.nav-item');
+    const title = document.querySelector('.nav-icon');
+
+    window.addEventListener('scroll', () => {
+
+    if(window.scrollY > 750){
+      navigation.classList.add('anim-nav');
+      logo.classList.add('anim-item');
+      title.classList.add('anim-icon');
+    } else {
+      navigation.classList.remove('anim-nav');
+      logo.classList.remove('anim-item');
+      title.classList.remove('anim-icon');
+    }
+  })
   }
 }
